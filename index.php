13b690433b6c5efc46e2c3afaf15a5a9690cd6e7
ismailorria <ismailorria@gmail.com>
2015-05-03 08:28:09 +0430

    Create index.php

---
+++ index.php
@@ -0,0 +1,403 @@
+<!DOCTYPE html>
+<!--[if IE 8]> <html lang="en" class="ie8"> <![endif]-->
+<!--[if IE 9]> <html lang="en" class="ie9"> <![endif]-->
+<!--[if !IE]><!--> <html lang="en"> <!--<![endif]-->
+<!-- BEGIN HEAD -->
+<?php
+include_once("header.php");
+
+
+?>
+                <!-- END LOGO SECTION -->
+             
+			<?php
+			include_once("navagation.php");
+			?>
+
+        </div>
+        <!-- END HEADER SECTION -->
+
+
+
+        <!-- MENU SECTION -->
+       <div id="left" >
+            <div class="media user-media well-small">
+                <a class="user-link" href="#">
+                    <img class="media-object img-thumbnail user-img" alt="User Picture" src="assets/img/user.gif" />
+                </a>
+                <br />
+                <div class="media-body">
+                    <h5 class="media-heading"> Joe Romlin</h5>
+                    <ul class="list-unstyled user-info">
+                        
+                        <li>
+                             <a class="btn btn-success btn-xs btn-circle" style="width: 10px;height: 12px;"></a> Online
+                           
+                        </li>
+                       
+                    </ul>
+                </div>
+                <br />
+            </div>
+
+            <?php
+			include_once("left_nav.php");
+			?>
+        </div>
+        <!--END MENU SECTION -->
+
+
+
+        <!--PAGE CONTENT -->
+        <div id="content">
+             
+            <div class="inner" style="min-height: 700px;">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1> Admin Dashboard </h1>
+                    </div>
+                </div>
+                  <hr />
+                 <!--BLOCK SECTION -->
+                 <div class="row">
+                    <div class="col-lg-12">
+                        <div style="text-align: center;">
+                           
+                            <a class="quick-btn" href="#">
+                                <i class="icon-check icon-2x"></i>
+                                <span> Products</span>
+                                <span class="label label-danger">2</span>
+                            </a>
+
+                            <a class="quick-btn" href="#">
+                                <i class="icon-envelope icon-2x"></i>
+                                <span>Messages</span>
+                                <span class="label label-success">456</span>
+                            </a>
+                            <a class="quick-btn" href="#">
+                                <i class="icon-signal icon-2x"></i>
+                                <span>Profit</span>
+                                <span class="label label-warning">+25</span>
+                            </a>
+                            <a class="quick-btn" href="#">
+                                <i class="icon-external-link icon-2x"></i>
+                                <span>value</span>
+                                <span class="label btn-metis-2">3.14159265</span>
+                            </a>
+                            <a class="quick-btn" href="#">
+                                <i class="icon-lemon icon-2x"></i>
+                                <span>tasks</span>
+                                <span class="label btn-metis-4">107</span>
+                            </a>
+                            <a class="quick-btn" href="#">
+                                <i class="icon-bolt icon-2x"></i>
+                                <span>Tickets</span>
+                                <span class="label label-default">20</span>
+                            </a>
+
+                            
+                            
+                        </div>
+
+                    </div>
+
+                </div>
+                  <!--END BLOCK SECTION -->
+                <hr />
+                   <!-- CHART & CHAT  SECTION -->
+                 <div class="row">
+                    <div class="col-lg-8">
+                        <div class="panel panel-default">
+                            
+                                                
+		
+                        </div>
+
+                    </div>
+
+                    
+                     <div class="col-lg-4">
+
+                        <div class="chat-panel panel panel-primary">
+                           
+
+                            
+
+                            <div class="panel-footer">
+                                <div class="input-group">
+                                    <input id="Text1" type="text" class="form-control input-sm" placeholder="Type your message here..." />
+                                    <span class="input-group-btn">
+                                        <button class="btn btn-warning btn-sm" id="Button1">
+                                            Send
+                                        </button>
+                                    </span>
+                                </div>
+                            </div>
+
+                        </div>
+
+
+                    </div>
+                </div>
+                 <!--END CHAT & CHAT SECTION -->
+                 <!-- COMMENT AND NOTIFICATION  SECTION -->
+                <div class="row">
+                    <div class="col-lg-7">
+
+                        <div class="chat-panel panel panel-success">
+                           
+
+                            
+
+                            
+
+                        </div>
+
+
+                    </div>
+                    <div class="col-lg-5">
+                         
+                      
+
+                          
+                    
+                    </div>
+                </div>
+                <!-- END COMMENT AND NOTIFICATION  SECTION -->
+                
+
+                
+
+                 <!--  STACKING CHART  SECTION   -->
+                <div class="row">
+                   <div class="col-lg-12">
+                    <div class="panel panel-default">
+                            <div class="panel-heading">
+                            Sales   Stacking
+                            </div>
+
+                            <div class="panel-body">
+                              
+			<div class="demo-container">
+			<div id="placeholderStack" class="demo-placeholder"></div>
+		</div>
+                                <p class="stackControls">
+			<button class="btn btn-primary" >With stacking</button>
+			<button class="btn btn-primary">Without stacking</button>
+		</p>
+
+		<p class="graphControls">
+			<button class="btn btn-primary">Bars</button>
+			<button class="btn btn-primary">Lines</button>
+			<button class="btn btn-primary">Lines with steps</button>
+		</p>
+		</div>
+		
+                            </div>
+                    </div>
+                     
+                </div>
+                 <!--END STACKING CHART SCETION  -->
+
+                 <!--TABLE, PANEL, ACCORDION AND MODAL  -->
+                          <div class="row">
+                    <div class="col-lg-6">
+                        <div class="box">
+                            <header>
+                                <h5>Simple Table</h5>
+                                <div class="toolbar">
+                                    <div class="btn-group">
+                                        <a href="#sortableTable" data-toggle="collapse" class="btn btn-default btn-sm accordion-toggle minimize-box">
+                                            <i class="icon-chevron-up"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </header>
+                            <div id="sortableTable" class="body collapse in">
+                                <table class="table table-bordered sortableTable responsive-table">
+                                    <thead>
+                                        <tr>
+                                            <th>#<i class="icon-sort"></i><i class="icon-sort-down"></i> <i class="icon-sort-up"></i></th>
+                                            <th>First Name<i class="icon-sort"></i><i class="icon-sort-down"></i> <i class="icon-sort-up"></i></th>
+                                            <th>Last Name<i class="icon-sort"></i><i class="icon-sort-down"></i> <i class="icon-sort-up"></i></th>
+                                            <th>Score<i class="icon-sort"></i><i class="icon-sort-down"></i> <i class="icon-sort-up"></i></th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+
+
+                                        <tr>
+                                            <td>1</td>
+                                            <td>Jill</td>
+                                            <td>Smith</td>
+                                            <td>50</td>
+                                        </tr>
+
+                                        <tr>
+                                            <td>2</td>
+                                            <td>Eve</td>
+                                            <td>Jackson</td>
+                                            <td>94</td>
+                                        </tr>
+
+                                        <tr>
+                                            <td>3</td>
+                                            <td>John</td>
+                                            <td>Doe</td>
+                                            <td>80</td>
+                                        </tr>
+
+                                        <tr>
+                                            <td>4</td>
+                                            <td>Adam</td>
+                                            <td>Johnson</td>
+                                            <td>67</td>
+                                        </tr>
+
+
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                        
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="panel panel-primary">
+                           
+                            
+                        </div>
+                        <div class="panel panel-default">
+                            
+                            <div class="panel-body">
+                                <div class="table-responsive">
+                                    <table class="table">
+                                        <thead>
+                                            <tr>
+                                                <th>#</th>
+                                                <th>First Name</th>
+                                                <th>Last Name</th>
+                                                <th>Username</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr class="success">
+                                                <td>1</td>
+                                                <td>Mark</td>
+                                                <td>Otto</td>
+                                                <td>@mdo</td>
+                                            </tr>
+                                            <tr class="info">
+                                                <td>2</td>
+                                                <td>Jacob</td>
+                                                <td>Thornton</td>
+                                                <td>@fat</td>
+                                            </tr>
+                                            <tr class="warning">
+                                                <td>3</td>
+                                                <td>Larry</td>
+                                                <td>the Bird</td>
+                                                <td>@twitter</td>
+                                            </tr>
+                                            <tr class="danger">
+                                                <td>4</td>
+                                                <td>John</td>
+                                                <td>Smith</td>
+                                                <td>@jsmith</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        </div>
+                    </div>
+
+                </div>
+                 <!--TABLE, PANEL, ACCORDION AND MODAL  -->
+
+                
+            </div>
+
+        </div>
+        <!--END PAGE CONTENT -->
+
+         <!-- RIGHT STRIP  SECTION -->
+        <div id="right">
+
+            
+            <div class="well well-small">
+                <ul class="list-unstyled">
+                    <li>Visitor &nbsp; : <span>23,000</span></li>
+                    <li>Users &nbsp; : <span>53,000</span></li>
+                    <li>Registrations &nbsp; : <span>3,000</span></li>
+                </ul>
+            </div>
+            <div class="well well-small">
+                <button class="btn btn-block"> Help </button>
+                <button class="btn btn-primary btn-block"> Tickets</button>
+                <button class="btn btn-info btn-block"> New </button>
+                <button class="btn btn-success btn-block"> Users </button>
+                <button class="btn btn-danger btn-block"> Profit </button>
+                <button class="btn btn-warning btn-block"> Sales </button>
+                <button class="btn btn-inverse btn-block"> Stock </button>
+            </div>
+            <div class="well well-small">
+                <span>Profit</span><span class="pull-right"><small>20%</small></span>
+
+                <div class="progress mini">
+                    <div class="progress-bar progress-bar-info" style="width: 20%"></div>
+                </div>
+                <span>Sales</span><span class="pull-right"><small>40%</small></span>
+
+                <div class="progress mini">
+                    <div class="progress-bar progress-bar-success" style="width: 40%"></div>
+                </div>
+                <span>Pending</span><span class="pull-right"><small>60%</small></span>
+
+                <div class="progress mini">
+                    <div class="progress-bar progress-bar-warning" style="width: 60%"></div>
+                </div>
+                <span>Summary</span><span class="pull-right"><small>80%</small></span>
+
+                <div class="progress mini">
+                    <div class="progress-bar progress-bar-danger" style="width: 80%"></div>
+                </div>
+            </div>
+          
+            
+         
+
+        </div>
+         <!-- END RIGHT STRIP  SECTION -->
+    </div>
+
+    <!--END MAIN WRAPPER -->
+
+    <!-- FOOTER -->
+    <div id="footer">
+        <p>&copy;  binarytheme &nbsp;2014 &nbsp;</p>
+    </div>
+    <!--END FOOTER -->
+
+
+    <!-- GLOBAL SCRIPTS -->
+    <script src="assets/plugins/jquery-2.0.3.min.js"></script>
+     <script src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/plugins/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+    <!-- END GLOBAL SCRIPTS -->
+
+    <!-- PAGE LEVEL SCRIPTS -->
+    <script src="assets/plugins/flot/jquery.flot.js"></script>
+    <script src="assets/plugins/flot/jquery.flot.resize.js"></script>
+    <script src="assets/plugins/flot/jquery.flot.time.js"></script>
+     <script  src="assets/plugins/flot/jquery.flot.stack.js"></script>
+    <script src="assets/js/for_index.js"></script>
+   
+    <!-- END PAGE LEVEL SCRIPTS -->
+
+
+</body>
+
+    <!-- END BODY -->
+</html>
